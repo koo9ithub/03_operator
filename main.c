c2d68d8a6c6feafa9934a41d77412214221bb3a6
koo9ithub <kk223704@naver.com>
2018-10-02 15:33:05 +0900

    03_operator.1

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int x;
+	int y;
+	
+	printf("input two integer:");
+	scanf("%i %i", &x, &y);
+	
+	printf("+ result is: %i\n", x+y);
+	printf("- result is: %i\n", x-y);
+	printf("* result is: %i\n", x*y);
+	printf("/ result is: %i\n", x/y);
+	printf("%% result is: %i\n", x%y);
+	
+	return 0;
+}
